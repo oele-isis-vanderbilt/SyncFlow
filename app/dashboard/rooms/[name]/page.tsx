d89e058ab2c35492fda9c7bbc4fc31b4c17e79e7
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-02-06 17:04:54 -0600

    Role based token generation

--- app/dashboard/rooms/[name]/page.tsx
+++ app/dashboard/rooms/[name]/page.tsx
@@ -2,21 +2,22 @@ import { lusitana } from '@/app/ui/fonts';
 import Room from '@/app/ui/dashboard/rooms/room';
 import { liveKitService } from '@/app/lib/livekit';
 import { useMemo } from 'react';
+import { auth } from "@/auth";
+import { Role } from '@prisma/client';
 
 export default async function Page({ params }: { params: { name: string } }) {
   const roomName = params.name;
-  let userName = 'admin';
+  const session = await auth();
+  const userName = session?.user?.name || 'Anonymous';
 
-  const token = await useMemo(async () => {
-    return await liveKitService.generateToken(userName, userName, {
+  const token = await liveKitService.generateToken(userName, userName, {
       canPublish: true,
-      canSubscribe: true,
+      canSubscribe: session?.user?.role === Role.ADMIN,
       canUpdateOwnMetadata: true,
       roomJoin: true,
-      roomCreate: false,
+      roomCreate: session?.user?.role === Role.ADMIN,
       room: roomName,
     });
-  }, [userName]);
 
   return (
     <main>
