848aab10721bfe1d29f80794cb65f45750f5901a
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-02-08 09:08:52 -0600

    Add `useState` hook  for disabling room create btn. Closes #2

--- app/ui/dashboard/create-room.tsx
+++ app/ui/dashboard/create-room.tsx
@@ -4,14 +4,19 @@ import { createRoom } from '@/app/lib/actions';
 
 import { PlusIcon } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { useState } from 'react';
 
 export default function Page() {
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
   return (
     <Button
       className="ml-10 rounded-md bg-teal-900 p-2 text-white"
       onClick={async () => {
+        setIsBtnDisabled(true);
         await createRoom();
+        setIsBtnDisabled(false);
       }}
+      aria-disabled={isBtnDisabled}
     >
       Create a new Room
       <PlusIcon className="ml-2 h-5 w-5 text-gray-50" />
