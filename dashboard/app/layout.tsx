d0cda550920cb715a368122f3ecfa1b1148cb3f1
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-04-11 10:40:59 -0500

    Set Preferred color scheme to dark

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={"dark"}>
       <body className={`${inter.className} flex h-screen flex-col`}>
         <div className="flex h-full flex-col">
           <div className="flex-1">{children}</div>
