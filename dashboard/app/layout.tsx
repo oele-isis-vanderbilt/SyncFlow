9973eeb40641534c22984a6fc2b760efd7a2bf2e
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-04-11 10:51:48 -0500

    Minimal changes to force dark mode

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={"dark"}>
+    <html lang="en">
       <body className={`${inter.className} flex h-screen flex-col`}>
         <div className="flex h-full flex-col">
           <div className="flex-1">{children}</div>
