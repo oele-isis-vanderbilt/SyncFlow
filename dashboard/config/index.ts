727f9a54a793ef7472ab0747343062a4fe865bd0
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-07-03 16:47:04 -0500

    Try fs based config reading

--- dashboard/config/index.ts
+++ dashboard/config/index.ts
@@ -1,5 +1,7 @@
 import type { DeploymentConfig } from '@/types/deployment-config-models';
 import { UserPermissions } from '@/types/deployment-config-models';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const environment = process.env.NODE_ENV || 'development';
 let config: DeploymentConfig | null = null;
@@ -9,13 +11,18 @@ export default function getConfig(): DeploymentConfig {
     return config;
   } else {
     try {
-      config = require(`./${environment}.json`) as DeploymentConfig;
+      config = JSON.parse(
+        fs.readFileSync(
+          path.join(process.cwd(), `/config/${environment}.json`),
+          'utf-8',
+        ),
+      );
       // Convert to enum
       if (config.userPermissions) {
         config.userPermissions = UserPermissions[config.userPermissions];
       }
     } catch (e) {
-      throw new Error(`Unknown environment: ${environment}`);
+      throw new Error(`Unknown environment: ${environment}: ${e}`);
     }
   }
   return config as DeploymentConfig;
